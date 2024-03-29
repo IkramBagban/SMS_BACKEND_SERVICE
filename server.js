c66910140b2e06e6860376688137fc479d228e90
moforemmanuel <moforemmanuel1000@gmail.com>
2024-03-29 10:42:20 +0100

    feat: update server

--- server.js
+++ server.js
@@ -1,66 +1,33 @@
 const express = require("express");
+const LoadBalancer = require("./models/loadbalancer");
 const { readQueue, writeQueue } = require("./utils/queue");
 const { TokenBucket } = require("./utils/rate-limiter");
+const { SMSProvider } = require("./providers/sms");
 
 const app = express();
 app.use(express.json());
 
-class SMSProvider {
-  constructor(name, throughput) {
-    this.name = name;
-    this.throughput = throughput;
-    this.isDown = false;
-    this.count = 0;
-    this.messageTimestamps = [];
-    this.tokenBucket = new TokenBucket(this.throughput, this.throughput / 60);
-  }
+const SMS_PROVIDERS = [
+  new SMSProvider("Airtel", 10),
+  new SMSProvider("Jio", 10),
+  new SMSProvider("VI", 10),
+];
 
-  async sendMessage(message) {
-    return new Promise((resolve, reject) => {
-      const isSuccess = Math.random() < 0.8; // Simulate 80% success rate
-      if (isSuccess) {
-        if (tokenBucket.consume(1)) {
-          // Consume 1 token for each successful message
-          resolve(`Message ${message.id} sent successfully`); // Resolve with receipt
-        } else {
-          console.log(`Rate limit exceeded. Delaying message #${message.id}`);
-          setTimeout(() => {
-            sendMessage(message).then(resolve).catch(reject); // Retry sending message after delay
-          }, 1000); // 1 second delay
+const loadBalancer = new LoadBalancer(SMS_PROVIDERS);
 
-          // Refill tokens during the delay
-          tokenBucket.refill();
-        }
-      } else {
-        // console.log("Message sending failed:", message);
-        reject(`Failed to send message ${message.id}`); // Reject with error message
-      }
-    });
+app.post("/send-sms", async (req, res) => {
+  const { messages } = req.body;
+  console.log("Round-robin distribution", messages.length);
 
-    async function sendMessages() {
-      try {
-        while (true) {
-          const queue = await readQueue(); // Fetch the queue data dynamically
-          if (queue.length === 0) {
-            console.log("Queue is empty. Exiting.");
-            break;
-          }
-          const message = queue.shift(); // Get the first message from the queue
-          try {
-            const data = await this.sendMessage(message); // Send the message asynchronously
-            console.log(data);
-          } catch (error) {
-            console.error(error);
-            // If sending the message fails, write it back to the queue
-            await writeQueue(message);
-          }
-        }
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-}
+  await loadBalancer.assignTask(messages);
+
+  const providerCounts = loadBalancer.getProviderCounts();
+  console.log("Provider Counts:", providerCounts);
+
+  res.send({ message: "Messages processing completed", providerCounts });
+
+  resetAllProviders();
+});
 
 const PORT = process.env.PORT || 3032;
 
