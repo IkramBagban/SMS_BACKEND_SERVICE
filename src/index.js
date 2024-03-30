556ecaec248fece5d1bb6501063d8fcdfbfcf518
moforemmanuel <moforemmanuel1000@gmail.com>
2024-03-30 16:49:55 +0100

    feat: new entry file

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
 import express from "express";
-import httpProxy from "http-proxy-middleware";
-import axios from "axios";
-import { createObjectCsvWriter } from "csv-writer";
 import { airtelProvider } from "./servers/airtel.js";
 import { jioProvider } from "./servers/jio.js";
 import { viProvider } from "./servers/vi.js";
+import { proxyRequest, selectedServerProxy } from "./middleware/proxy.js";
+import { WeightedRoundRobinBalancer } from "./utils/load-balancer.js";
+import { logger } from "./middleware/logger.js";
+import Peers from "weighted-round-robin";
 
 const servers = [airtelProvider, jioProvider, viProvider];
 const throughputs = servers.map((server) => server.throughput);
 
 const calculateWeight = (throughput) =>
   Math.floor(throughput / Math.min(...throughputs));
-
-// const serverConfigurations = [
-//   { port: 5073, weight: 2 },
-//   { port: 5173, weight: 1 },
-//   { port: 5273, weight: 2 },
-//   { port: 5373, weight: 1 },
-//   { port: 5473, weight: 2 },
-//   { port: 5573, weight: 1 },
-//   { port: 5673, weight: 2 },
-//   { port: 5773, weight: 1 },
-//   { port: 5873, weight: 2 },
-//   { port: 5973, weight: 1 },
-// ];
 
 const serverConfigurations = servers.map((server) => ({
   port: server.port,
@@ -32,41 +20,30 @@ const serverConfigurations = servers.map((server) => ({
 
 const app = express();
 const port = 8000;
+const balancer = new WeightedRoundRobinBalancer(serverConfigurations);
+const peers = new Peers();
 
-// Create a CSV writer to log requests
-const csvWriter = createObjectCsvWriter({
-  path: "request_logs.csv",
-  header: [
-    { id: "timestamp", title: "Timestamp" },
-    { id: "ip", title: "IP Address" },
-    { id: "serverPort", title: "Server Port" },
-    { id: "originalUrl", title: "Original URL" },
-  ],
-});
+serverConfigurations.forEach(({ port, weight }) =>
+  peers.add({
+    server: `http://localhost:${port}`,
+    weight,
+  })
+);
 
 // Middleware to randomly select a server using Weighted Round Robin algorithm
 app.use((req, res, next) => {
-  const selectedServer = selectServer(serverConfigurations);
+  // const selectedServer = balancer.selectServer(serverConfigurations);
+  const selectedServer = peers.get();
   req.selectedServer = selectedServer;
+
   // Log the request information to CSV
-  const logData = {
-    timestamp: new Date().toISOString(),
-    ip: req.ip,
-    serverPort: selectedServer ? selectedServer.port : "N/A",
-    originalUrl: req.originalUrl,
-  };
-  csvWriter.writeRecords([logData]);
+  logger(req, selectedServer);
+
   if (selectedServer) {
     proxyRequest(req, res);
   } else {
     res.status(503).send("Service Unavailable");
   }
-});
-
-// Proxy middleware for the selected server
-const selectedServerProxy = httpProxy.createProxyMiddleware({
-  target: "<http://localhost>", // Set the target to the base URL of your servers
-  changeOrigin: true,
 });
 
 // Use the proxy middleware for all routes
@@ -75,91 +52,3 @@ app.use("/", selectedServerProxy);
 app.listen(port, () => {
   console.log(`Load Balancer listening at <http://localhost>:${port}`);
 });
-
-// Function to select a server using Weighted Round Robin
-// function selectServer(serverConfigurations) {
-//   let currentWeightIndex = 0;
-//   while (true) {
-//     const totalWeight = serverConfigurations.reduce(
-//       (acc, config) => acc + config.weight,
-//       0
-//     );
-//     const randomNum = Math.floor(Math.random() * totalWeight);
-//     let weightSum = 0;
-//     for (let i = currentWeightIndex; i < serverConfigurations.length; i++) {
-//       const config = serverConfigurations[i];
-//       weightSum += config.weight;
-//       if (randomNum < weightSum) {
-//         currentWeightIndex = (i + 1) % serverConfigurations.length; // Update index for the next round
-//         return config;
-//       }
-//     }
-//     currentWeightIndex = 0;
-//   }
-// }
-
-let currentIndex = -1;
-let currentWeight = 0;
-let maxWeight = 0;
-let gcd = 0;
-
-// Find the greatest common divisor
-serverConfigurations.forEach((config) => {
-  maxWeight = Math.max(maxWeight, config.weight);
-});
-for (let i = 0; i < serverConfigurations.length; i++) {
-  if (gcd === 0) {
-    gcd = serverConfigurations[i].weight;
-  } else {
-    gcd = gcd_func(gcd, serverConfigurations[i].weight);
-  }
-}
-
-function gcd_func(a, b) {
-  if (!b) {
-    return a;
-  }
-  return gcd_func(b, a % b);
-}
-
-function nextServer() {
-  while (true) {
-    currentIndex = (currentIndex + 1) % serverConfigurations.length;
-    if (currentIndex === 0) {
-      currentWeight = currentWeight - gcd;
-      if (currentWeight <= 0) {
-        currentWeight = maxWeight;
-        if (currentWeight === 0) {
-          return null; // no server available
-        }
-      }
-    }
-    if (serverConfigurations[currentIndex].weight >= currentWeight) {
-      return serverConfigurations[currentIndex];
-    }
-  }
-}
-
-// Function to select a server using Weighted Round Robin
-function selectServer() {
-  const server = nextServer();
-  return server;
-}
-
-// Function to proxy the request to the selected server
-function proxyRequest(req, res) {
-  const selectedServer = req.selectedServer;
-  const selectedPort = selectedServer.port;
-  const originalUrl = req.originalUrl; // Store original URL
-  axios
-    .get(`http://localhost:${selectedPort}${originalUrl}`)
-    .then((serverResponse) => {
-      const responseData = serverResponse.data; // Get response data from server
-      // Send response data to client
-      res.send(responseData);
-    })
-    .catch((error) => {
-      console.error(`Error forwarding request to server: ${error}`);
-      res.status(500).send("Internal Server Error");
-    });
-}
