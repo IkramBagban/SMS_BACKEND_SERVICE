2219e882ea2535ab64cec8861aa7ae31367d879d
moforemmanuel <moforemmanuel1000@gmail.com>
2024-03-30 16:46:31 +0100

    feat: add csv logger

---
+++ src/middleware/logger.js
@@ -0,0 +1,25 @@
+import { createObjectCsvWriter } from "csv-writer";
+
+// Create a CSV writer to log requests
+const csvWriter = createObjectCsvWriter({
+  path: "request_logs.csv",
+  header: [
+    { id: "timestamp", title: "Timestamp" },
+    { id: "ip", title: "IP Address" },
+    // { id: "serverPort", title: "Server Port" },
+    { id: "server", title: "Server" },
+    { id: "originalUrl", title: "Original URL" },
+  ],
+});
+
+// Log the request information to CSV
+export const logger = (req, selectedServer) => {
+  const logData = {
+    timestamp: new Date().toISOString(),
+    ip: req.ip,
+    serverPort: selectedServer ? selectedServer.port : "N/A",
+    server: selectedServer ? selectedServer.server : "N/A",
+    originalUrl: req.originalUrl,
+  };
+  csvWriter.writeRecords([logData]);
+};
