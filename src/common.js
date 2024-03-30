a646967021c520151783bc4aaedaa78046e478e2
moforemmanuel <moforemmanuel1000@gmail.com>
2024-03-30 19:39:15 +0100

    add middlewares

--- src/common.js
+++ src/common.js
@@ -1,9 +1,39 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 
 export function createServer(provider) {
   const app = express();
 
+  const limiter = rateLimit({
+    windowMs: 10 * 1000, // 1 minute
+    // limit: provider.throughput, // Limit each server to #throughput requests per `window` (here, per 15 minutes).
+    limit: 2,
+    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    message: `You have exceeded your ${provider.throughput} requests per minute limit.}`,
+    handler: (req, res, next, options) => {
+      console.log("Queueing request");
+      provider.queue.push({ req, res });
+      next();
+    },
+  });
+
+  app.use(limiter);
   app.use(express.json());
+
+  // Middleware to store request timestamps
+  app.use((req, res, next) => {
+    // Store the timestamp of the current request
+    const currentTime = Date.now();
+    provider.requestTimestamps.push(currentTime);
+
+    // Remove timestamps older than the window period
+    provider.requestTimestamps = provider.requestTimestamps.filter(
+      (timestamp) => currentTime - timestamp < 60 * 1000
+    );
+
+    next(); // Continue processing the request
+  });
 
   app.get("/", (req, res) => {
     res.json({
@@ -21,6 +51,24 @@ export function createServer(provider) {
     } catch (err) {
       console.log(err);
       res.status(err.code ?? 500).json(err);
+    }
+  });
+
+  // Process queued requests if rate limit allows
+  app.use((req, res, next) => {
+    const currentTime = Date.now();
+    // Remove expired timestamps from the array
+    provider.requestTimestamps = provider.requestTimestamps.filter(
+      (timestamp) => currentTime - timestamp < 60 * 1000
+    );
+    // If rate limit allows, process the queued request
+    if (provider.requestTimestamps.length < provider.throughput) {
+      const { req: queuedReq, res: queuedRes } = provider.queue.shift(); // Dequeue the request
+      // Handle the request
+      console.log("Processing queued request");
+      queuedRes.send("Queued request processed successfully.");
+      // Record the timestamp of the processed request
+      provider.requestTimestamps.push(currentTime);
     }
   });
 
