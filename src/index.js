e807a0d5395c5517c3df37fa62de8bea89706980
moforemmanuel <moforemmanuel1000@gmail.com>
2024-03-30 18:26:16 +0100

    remove double proxy req

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { viProvider } from "./servers/vi.js";
 import { proxyRequest, selectedServerProxy } from "./middleware/proxy.js";
 import { WeightedRoundRobinBalancer } from "./utils/load-balancer.js";
 import { logger } from "./middleware/logger.js";
-import Peers from "weighted-round-robin";
+import bodyParser from "body-parser";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const servers = [airtelProvider, jioProvider, viProvider];
 const throughputs = servers.map((server) => server.throughput);
@@ -15,39 +16,43 @@ const calculateWeight = (throughput) =>
 
 const serverConfigurations = servers.map((server) => ({
   port: server.port,
+  server: `http://localhost:${server.port}`,
   weight: calculateWeight(server.throughput),
 }));
 
 const app = express();
 const port = 8000;
 const balancer = new WeightedRoundRobinBalancer(serverConfigurations);
-const peers = new Peers();
 
-serverConfigurations.forEach(({ port, weight }) =>
-  peers.add({
-    server: `http://localhost:${port}`,
-    weight,
-  })
-);
+app.use(bodyParser.json());
 
 // Middleware to randomly select a server using Weighted Round Robin algorithm
 app.use((req, res, next) => {
-  // const selectedServer = balancer.selectServer(serverConfigurations);
-  const selectedServer = peers.get();
+  const selectedServer = balancer.selectServer(serverConfigurations);
   req.selectedServer = selectedServer;
 
   // Log the request information to CSV
   logger(req, selectedServer);
 
-  if (selectedServer) {
-    proxyRequest(req, res);
-  } else {
-    res.status(503).send("Service Unavailable");
-  }
+  // if (selectedServer) {
+  //   proxyRequest(req, res);
+  // } else {
+  //   res.status(503).send("Service Unavailable");
+  // }
+
+  next();
 });
 
 // Use the proxy middleware for all routes
 app.use("/", selectedServerProxy);
+
+// Proxy middleware configuration
+// app.use("/", (req, res) => {
+//   createProxyMiddleware({
+//     target: `http://localhost:${req.selectedServer.port}`,
+//     changeOrigin: true,
+//   })(req, res);
+// });
 
 app.listen(port, () => {
   console.log(`Load Balancer listening at <http://localhost>:${port}`);
