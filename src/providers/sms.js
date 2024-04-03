92d0f41af82960482fd880b8f61b269e7e9eaaa5
moforemmanuel <moforemmanuel1000@gmail.com>
2024-04-03 11:42:35 +0100

    update sms provider

--- src/providers/sms.js
+++ src/providers/sms.js
@@ -1,3 +1,5 @@
+import Queue from "bull";
+
 export class SMSProvider {
   constructor(name, throughput, port) {
     this.port = port;
@@ -5,36 +7,9 @@ export class SMSProvider {
     this.throughput = throughput;
     this.status = "idle";
     this.count = 0;
-    this.queue = [];
+    this.queue = new Queue("requestQueue");
     this.requestTimestamps = [];
-    // this.tokenBucket = new TokenBucket(this.throughput, this.throughput / 60);
   }
-
-  // async sendMessage(message) {
-  //   return new Promise((resolve, reject) => {
-  //     const isSuccess = Math.random() < 0.8; // Simulate 80% success rate
-  //     if (isSuccess) {
-  //       if (tokenBucket.consume(1)) {
-  //         // Consume 1 token for each successful message
-  //         resolve({
-  //           message: `Message ${message.id} sent successfully`,
-  //           senderName: this.name,
-  //         }); // Resolve with receipt
-  //       } else {
-  //         console.log(`Rate limit exceeded. Delaying message #${message.id}`);
-  //         setTimeout(() => {
-  //           sendMessage(message).then(resolve).catch(reject); // Retry sending message after delay
-  //         }, 1000); // 1 second delay
-
-  //         // Refill tokens during the delay
-  //         tokenBucket.refill();
-  //       }
-  //     } else {
-  //       // console.log("Message sending failed:", message);
-  //       reject(`Failed to send message ${message.id}`); // Reject with error message
-  //     }
-  //   });
-  // }
 
   async sendMessage(message) {
     return new Promise((resolve, reject) => {
@@ -59,28 +34,4 @@ export class SMSProvider {
       }
     });
   }
-
-  // async sendMessages() {
-  //   try {
-  //     while (true) {
-  //       const queue = await readQueue(); // Fetch the queue data dynamically
-  //       if (queue.length === 0) {
-  //         console.log("Queue is empty. Exiting.");
-  //         this.status = "idle";
-  //         break;
-  //       }
-  //       const message = queue.shift(); // Get the first message from the queue
-  //       try {
-  //         const data = await this.sendMessage(message); // Send the message asynchronously
-  //         console.log(data);
-  //       } catch (error) {
-  //         console.error(error);
-  //         // If sending the message fails, write it back to the queue
-  //         await writeQueue(message);
-  //       }
-  //     }
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }
 }
